72e318eeda4c6c426ea4d762826e0a1774b39183
Clive Tsotsobe <tsotsobec@gmail.com>
2023-06-15 17:56:12 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program that prints a string using the puts function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
