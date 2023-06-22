6d95fac4195aada0ceaa794333f1a10b02c30ade
Clive Tsotsobe <tsotsobec@gmail.com>
2023-06-22 13:38:35 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,6 @@
 /**
  * print_most_numbers - prints numbers from 0 to 9
  * Do not print 2 and 4
- * 
  * Return: void
  */
 
@@ -11,10 +10,10 @@ void print_most_numbers(void)
 {
 	char c;
 
-	for (c = '0'; c = <= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		if (!(c == '2' || c == '4'))
-			_putchar(c);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
