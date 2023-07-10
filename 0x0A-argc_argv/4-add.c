1eed111f3fd71ec19b17fb4fd509c47c376deb48
Clive Tsotsobe <tsotsobec@gmail.com>
2023-07-10 12:41:05 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,26 +12,23 @@
 
 int check_num(char *str)
 {
-	/*declaring variables*/
 	unsigned int count;
 
-	count = 0
-		while (count < strlen(str))/*count string*/
-
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
 		{
-			if (!isdigit(str[count]))/*check if str there are digit*/
-			{
-				return (0);
-			}
-
-			count++;
+			return (0);
 		}
+		count++;
+	}
 	return (1);
 }
 
 /**
  * main - program that adds positive numbers
- * @argc: count arguments
+ * @argc: Count arguments
  * @argv: arguments
  *
  * Return: Always 0 (Success)
@@ -39,32 +36,26 @@ int check_num(char *str)
 
 int main(int argc, char *argv[])
 {
-	/*Declaring variables*/
 	int count;
 	int str_to_int;
 	int sum = 0;
 
 	count = 1;
-	while (count < argc)/*Goes through the whole array*/
+	while (count < argc)
 	{
 		if (check_num(argv[count]))
-
 		{
-			str_to_int = atoi(argv[count]);/*ATOI --> convert string to int*/
+			str_to_int = atoi(argv[count]);
 			sum += str_to_int;
 		}
-
-		/*Condition if one of the number contains symbols that are not digits*/
 		else
 		{
 			printf("Error\n");
-		return (1);
+			return (1);
 		}
-
 		count++;
 	}
-
-	printf("%d\n", sum);/*print sum*/
+	printf("%d\n", sum);
 
 	return (0);
 }
