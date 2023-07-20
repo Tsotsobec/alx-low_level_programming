44de82e7c1911bcfd92d5f23727264ab1feb6027
Clive Tsotsobe <tsotsobec@gmail.com>
2023-07-20 12:08:28 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -12,7 +12,7 @@
 int main(int argc, char *argv[])
 {
 	int bytes, i;
-	char *arr;
+	unsigned char *arr;
 
 	if (argc != 2)
 	{
@@ -22,13 +22,13 @@ int main(int argc, char *argv[])
 
 	bytes = atoi(argv[1]);
 
-	if (bytes < 0)
+	if (bytes <= 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 
-	arr = (char *)main;
+	arr = (unsigned char *)main;
 
 	for (i = 0; i < bytes; i++)
 	{
